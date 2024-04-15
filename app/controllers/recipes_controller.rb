1a038386da3d90d2a1c6e8c1dc77f72b49241d10
山本裕一朗 <udyh3nm3ii93n9y9s8hb@docomo.ne.jp>
2024-04-15 01:57:56 -0700

    新着レシピ一覧のレイアウトを仮にだが整えた

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,7 @@ class RecipesController < ApplicationController
 
 	def new
 		@recipe = Recipe.new
+		set_step_build
 	end
 
 	def create
@@ -34,5 +35,12 @@ class RecipesController < ApplicationController
   # Strong Parameters
   def recipe_params
     params.require(:recipe).permit(:name, :thumbnail, :thumbnail_edited, :bio, :copy_permission, steps_attributes: [:id, :number, :process])
+  end
+
+	def set_step_build
+    6.times {
+      #@process_number += 1
+			@steps = @recipe.steps.build
+		}
   end
 end
