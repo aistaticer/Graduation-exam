b4b30a53398b3fe125eaf2b95d966604d814984d
山本裕一朗 <yuu@yamamotohiroshiichirounoMacBook-Air.local>
2024-04-15 03:03:18 -0700

    レシピの手順のフォームを６つ書き込めるように調整した

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
 	has_many :steps, dependent: :destroy
 	has_one_attached :thumbnail, dependent: :destroy
+  accepts_nested_attributes_for :steps
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,2 +1,4 @@
 class Step < ApplicationRecord
+	belongs_to :recipe
+	validates :process, presence: true, length: { maximum: 255 }
 end
