8d3dd10c6b803de5e87a20a1b700cbcef8c8b850
山本裕一朗 <yuu@yamamotohiroshiichirounoMacBook-Air.local>
2024-04-19 05:25:21 -0700

    pagenationの機能を実装しました

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,8 @@
+class LikesController < ApplicationController
+	def index
+		recipe_ids = StampMiddle.where(user_id: current_user.id).pluck(:recipe_id)
+		@liked_recipes = Recipe.where(id: recipe_ids).with_attached_thumbnail
+		StampMiddle.liked_by_user?(@liked_recipes,current_user.id)
+		StampMiddle.count_like_recipe(@liked_recipes)
+	end
+end
