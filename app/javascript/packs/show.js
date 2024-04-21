c1ac903334d13645f082a14303d4cb5a8fbe0cc6
aistaticer <udyh3nm3ii93n9y9s8hb@docomo.ne.jp>
2024-04-21 16:49:09 +0900

    コメントのレイアウトを少し整えた

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,5 +1,5 @@
-function comment_replyform_look(commentId){
-  let comment_replyform = document.getElementById('comment_replyForm_' + commentId)
+function comment_replyform_look(commentId,classname){
+  let comment_replyform = document.getElementById(classname + commentId)
   if(comment_replyform.className == "show"){
     comment_replyform.classList.add("hide");
     comment_replyform.classList.remove("show");
@@ -21,11 +21,23 @@ document.addEventListener('DOMContentLoaded', function() {
       // このアイコンが持つdata-comment-idの値を取得
       var commentId = icon.getAttribute('data-comment-id');
       // コンソールに表示
-      comment_replyform_look(commentId);
+      comment_replyform_look(commentId, 'comment_replyForm_');
       console.log('コメントID:', commentId);
       console.log("クリックを感知")
     });
   });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("アイコンからコメントIDを受け取る関数");
+  var iconreplies_all = document.querySelectorAll('.icon-replies');
+
+  iconreplies_all.forEach(function(iconreplies){
+    iconreplies.addEventListener('click', function(e) {
+      var commentId = iconreplies.getAttribute('data-comment-id');
+      comment_replyform_look(commentId,"replies_");
+    })
+  })
 });
 
 
