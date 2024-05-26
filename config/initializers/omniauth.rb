42bbc8489eded2be33562760d11338835e7aa63b
aistaticer <udyh3nm3ii93n9y9s8hb@docomo.ne.jp>
2024-05-27 00:27:16 +0900

    googleアカウントのログイン機能を追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  OmniAuth.config.allowed_request_methods = [:post, :get]
+  #OmniAuth.config.allowed_request_methods = [:post, :get]
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     redirect_uri: "https://evolution-recipes-949263457034.herokuapp.com/users/auth/google_oauth2/callback"
   }
---
+++ config/session_store.rb
@@ -0,0 +1,5 @@
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: '_my_session', expire_after: 1.weeks, domain: 'https://evolution-recipes-949263457034.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_my_session', domain: 'localhost'
+end
