3d208849cbb21b55f7b0caa794d231d9df5df4eb
aistaticer <udyh3nm3ii93n9y9s8hb@docomo.ne.jp>
2024-05-27 01:20:47 +0900

    session_store.rbを書いた

--- config/session_store.rb
+++ config/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_my_session', expire_after: 1.weeks, domain: 'evolution-recipes'
+  Rails.application.config.session_store :cookie_store, key: '_my_session', expire_after: 1.weeks, domain: 'evolution-recipes-949263457034.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_my_session', domain: 'localhost:3000/'
 end
