76b824fff5c2323442d5798d76c65d6a382dde18
aistaticer <udyh3nm3ii93n9y9s8hb@docomo.ne.jp>
2024-06-18 01:06:39 +0900

    editを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def show
     @url_user_show = true;
     @user = User.includes(:recipes).find(params[:id])
-    StampMiddle.count_like_recipe(@user.recipes)
+
+    delicious_type = StampsType.find_by(name: "Delicious")
+    like_type = StampsType.find_by(name: "Like")
+    StampMiddle.stamped_by_user?(@user.recipes,current_user.id,delicious_type,like_type)
+    StampMiddle.count_stamp_recipe(@user.recipes,delicious_type,like_type)
   end
 end
